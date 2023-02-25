c0b005ddf61517fdf6d1b5ab6500878fe137ccc6
anitamalina <anitatranberg@gmail.com>
2023-02-25 19:59:37 +0100

    responsive optimiced

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,20 @@
-import type { AppProps } from 'next/app'
-import { useState, useEffect, ChangeEvent, useRef } from 'react'
-import './App.css'
-import { Configuration, OpenAIApi } from 'openai'
-import getConfig from 'next/config'
+import type { AppProps } from "next/app";
+import { useState, useEffect, ChangeEvent, useRef } from "react";
+import "./App.css";
+import { Configuration, OpenAIApi } from "openai";
+import getConfig from "next/config";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [imgURL, setImgURL] = useState('')
-  const [userTextInput, setUserTextInput] = useState('')
-  const [count, setCount] = useState(0)
-  const [imageTitel, setImageTitle] = useState('')
-  const [loading, setLoading] = useState(false)
-  const [errorMessage, setErrorMessage] = useState('')
-  const [requestErrorMessage, setRequestErrorMessage] = useState('')
+  const [imgURL, setImgURL] = useState("");
+  const [userTextInput, setUserTextInput] = useState("");
+  const [count, setCount] = useState(0);
+  const [imageTitel, setImageTitle] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [requestErrorMessage, setRequestErrorMessage] = useState("");
   /*   const [typedText, setTypedText] = useState('') */
 
-  const text = 'Generating image...'
+  const text = "Generating image...";
 
   let verbs: string[],
     nouns: string[],
@@ -22,186 +22,186 @@ export default function App({ Component, pageProps }: AppProps) {
     preposition: string[],
     place: string[],
     preposition2: string[],
-    item: string[]
+    item: string[];
   nouns = [
-    'bird',
-    'boy',
-    'duck',
-    'rhinoceros',
-    'hamster',
-    'dog',
-    'bunny',
-    'cat',
-    'mouse',
-    'girl',
-  ]
+    "bird",
+    "boy",
+    "duck",
+    "rhinoceros",
+    "hamster",
+    "dog",
+    "bunny",
+    "cat",
+    "mouse",
+    "girl",
+  ];
   verbs = [
-    'kicked',
-    'ran',
-    'flew',
-    'drank',
-    'sliced',
-    'rolled',
-    'breathed',
-    'jumped',
-    'ate',
-    'shopped',
-  ]
+    "kicked",
+    "ran",
+    "flew",
+    "drank",
+    "sliced",
+    "rolled",
+    "breathed",
+    "jumped",
+    "ate",
+    "shopped",
+  ];
   adjectives = [
-    'A beautiful',
-    'A fancy',
-    'A dancing',
-    'A lovely',
-    'A cute',
-    'An elegant',
-    'A mysterious',
-    'A hot',
-    'A dirty',
-    'A slimy',
-  ]
+    "A beautiful",
+    "A fancy",
+    "A dancing",
+    "A lovely",
+    "A cute",
+    "An elegant",
+    "A mysterious",
+    "A hot",
+    "A dirty",
+    "A slimy",
+  ];
   preposition = [
-    'down',
-    'into',
-    'up',
-    'on',
-    'upon',
-    'below',
-    'above',
-    'through',
-    'across',
-    'towards',
-  ]
+    "down",
+    "into",
+    "up",
+    "on",
+    "upon",
+    "below",
+    "above",
+    "through",
+    "across",
+    "towards",
+  ];
   place = [
-    'the moonlight',
-    'the universe',
-    'the beach',
-    'the mountains',
-    'the water',
-    'the ocean',
-    'Mars',
-    'Earth',
-    'Sahara',
-    'clouds',
-  ]
+    "the moonlight",
+    "the universe",
+    "the beach",
+    "the mountains",
+    "the water",
+    "the ocean",
+    "Mars",
+    "Earth",
+    "Sahara",
+    "clouds",
+  ];
   preposition2 = [
-    'with',
-    'on',
-    'close to',
-    'behind',
-    'upon',
-    'below',
-    'above',
-    'across',
-    'towards',
-    'next to',
-  ]
+    "with",
+    "on",
+    "close to",
+    "behind",
+    "upon",
+    "below",
+    "above",
+    "across",
+    "towards",
+    "next to",
+  ];
   item = [
-    'a coffee cup',
-    'a shoe',
-    'a star',
-    'a suitcase',
-    'a drone',
-    'a robot',
-    'an elefant',
-    'a diamond',
-    'a magic wond',
-    'a lollipop',
-  ]
+    "a coffee cup",
+    "a shoe",
+    "a star",
+    "a suitcase",
+    "a drone",
+    "a robot",
+    "an elefant",
+    "a diamond",
+    "a magic wond",
+    "a lollipop",
+  ];
 
-  const { publicRuntimeConfig } = getConfig()
+  const { publicRuntimeConfig } = getConfig();
 
   const apiKey =
-    typeof publicRuntimeConfig !== 'undefined' && publicRuntimeConfig.apiKey
+    typeof publicRuntimeConfig !== "undefined" && publicRuntimeConfig.apiKey
       ? publicRuntimeConfig.apiKey
-      : process.env.OPENAI_API_KEY
+      : process.env.OPENAI_API_KEY;
 
   if (!apiKey) {
-    throw new Error('apiKey is not defined in config file')
+    throw new Error("apiKey is not defined in config file");
   }
 
-  const configuration = new Configuration({ apiKey })
-  const openai = new OpenAIApi(configuration)
+  const configuration = new Configuration({ apiKey });
+  const openai = new OpenAIApi(configuration);
 
   const generateImage = async () => {
     if (requestErrorMessage) {
-      setRequestErrorMessage('')
+      setRequestErrorMessage("");
     }
-    setLoading(true)
-    setImageTitle(userTextInput)
-    let response: any
+    setLoading(true);
+    setImageTitle(userTextInput);
+    let response: any;
     try {
       response = await openai.createImage({
         prompt: userTextInput,
         n: 1,
-        size: '1024x1024',
-      })
-      const image_url = response.data.data[0].url
-      setImgURL(image_url || 'no image found')
+        size: "1024x1024",
+      });
+      const image_url = response.data.data[0].url;
+      setImgURL(image_url || "no image found");
     } catch (e: any) {
-      setRequestErrorMessage(e.response.data.error.message)
-      setImgURL('')
+      setRequestErrorMessage(e.response.data.error.message);
+      setImgURL("");
     } finally {
-      setLoading(false)
-      setUserTextInput('')
-      setCount(0)
+      setLoading(false);
+      setUserTextInput("");
+      setCount(0);
     }
-  }
+  };
 
   const generateRandomImage = async () => {
     if (requestErrorMessage) {
-      setRequestErrorMessage('')
+      setRequestErrorMessage("");
     }
     if (errorMessage) {
-      setErrorMessage('')
+      setErrorMessage("");
     }
-    const sentence = generateRandomSentence()
-    setImageTitle(sentence)
-    setLoading(true)
+    const sentence = generateRandomSentence();
+    setImageTitle(sentence);
+    setLoading(true);
     const response = await openai.createImage({
       prompt: sentence,
       n: 1,
-      size: '1024x1024',
-    })
-    setLoading(false)
-    const image_url = response.data.data[0].url
-    setImgURL(image_url || 'no image found')
-  }
+      size: "1024x1024",
+    });
+    setLoading(false);
+    const image_url = response.data.data[0].url;
+    setImgURL(image_url || "no image found");
+  };
 
   const generateRandomSentence = () => {
-    var rand1 = Math.floor(Math.random() * 10)
-    var rand2 = Math.floor(Math.random() * 10)
-    var rand3 = Math.floor(Math.random() * 10)
-    var rand4 = Math.floor(Math.random() * 10)
-    var rand5 = Math.floor(Math.random() * 10)
-    var rand6 = Math.floor(Math.random() * 10)
-    var rand7 = Math.floor(Math.random() * 10)
+    var rand1 = Math.floor(Math.random() * 10);
+    var rand2 = Math.floor(Math.random() * 10);
+    var rand3 = Math.floor(Math.random() * 10);
+    var rand4 = Math.floor(Math.random() * 10);
+    var rand5 = Math.floor(Math.random() * 10);
+    var rand6 = Math.floor(Math.random() * 10);
+    var rand7 = Math.floor(Math.random() * 10);
 
     var sentence =
       adjectives[rand1] +
-      ' ' +
+      " " +
       nouns[rand2] +
-      ' ' +
+      " " +
       verbs[rand3] +
-      ' ' +
+      " " +
       preposition[rand4] +
-      ' ' +
+      " " +
       place[rand5] +
-      ' ' +
+      " " +
       preposition2[rand6] +
-      ' ' +
+      " " +
       item[rand7] +
-      '.'
+      ".";
 
-    return sentence
-  }
+    return sentence;
+  };
 
   const handleUserInput = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    setUserTextInput(e.target.value)
+    setUserTextInput(e.target.value);
     if (errorMessage) {
-      setErrorMessage('')
+      setErrorMessage("");
     }
-    setCount(e.target.value.length)
-  }
+    setCount(e.target.value.length);
+  };
 
   /*   useEffect(() => {
     if (loading) {
@@ -218,18 +218,18 @@ export default function App({ Component, pageProps }: AppProps) {
     }
   }, [loading]) */
 
-  const sendEmail = (url = '') => {
-    url = imgURL
-    const message = `Here is your image download link: ${url}`
-    window.location.href = `mailto: someone@example.com?subject=Image Download Link&body=${message}`
-  }
+  const sendEmail = (url = "") => {
+    url = imgURL;
+    const message = `Here is your image download link: ${url}`;
+    window.location.href = `mailto: someone@example.com?subject=Image Download Link&body=${message}`;
+  };
 
   const emptyInputError = () => {
-    setErrorMessage('You need to write a text to generate an image.')
+    setErrorMessage("You need to write a text to generate an image.");
     if (requestErrorMessage) {
-      setRequestErrorMessage('')
+      setRequestErrorMessage("");
     }
-  }
+  };
 
   return (
     <div className="app-main">
@@ -244,7 +244,7 @@ export default function App({ Component, pageProps }: AppProps) {
         />
         <p className="small-text">{count} / 1000</p>
       </div>
-      {userTextInput === '' ? (
+      {userTextInput === "" ? (
         <>
           <button className="inactive-btn" onClick={() => emptyInputError()}>
             Generate Image
@@ -272,18 +272,18 @@ export default function App({ Component, pageProps }: AppProps) {
         ) : imgURL ? (
           <>
             <h4>{imageTitel}</h4>
-            <img
-              src={imgURL}
-              onClick={() => window.open(imgURL)}
-              style={{ cursor: 'pointer' }}
-              alt="Generated Image"
-            />
+              <img
+                src={imgURL}
+                onClick={() => window.open(imgURL)}
+                style={{ cursor: "pointer" }}
+                alt="Generated Image"
+              />
           </>
         ) : (
           <></>
         )}
       </>
-      <p className='errorMessageRequest'>{requestErrorMessage}</p>
+      <p className="errorMessageRequest">{requestErrorMessage}</p>
     </div>
-  )
+  );
 }
